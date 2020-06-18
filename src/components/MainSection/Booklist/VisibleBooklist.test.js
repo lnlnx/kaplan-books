2c2b78b3d4b66a36c8f89b740e0edc5ba85236d7
Tingting <ttina.chenn@gmail.com>
2020-06-18 12:13:43 -0400

    Added getVisibleBooks function to filter book list by search string

---
+++ src/components/MainSection/Booklist/VisibleBooklist.test.js
@@ -0,0 +1,25 @@
+import {getVisibleBooks} from './VisibleBooklist';
+
+describe("getVisibleBooks", () => {
+    test("should filter books by search string and return list of books with matched parts", () => {
+        const testBooks = [
+            {id: "1", authors: "author1", title: "title", publisher: "some"},
+            {id: "2", authors: "author1", title: "title2", publisher: "some title"},
+            {id: "3", authors: "author1", title: "test", publisher: "some"},
+        ]
+        expect(getVisibleBooks(testBooks, "some")).toEqual(
+            [
+                {id: "1", authors: "author1", title: "title", publisher: "some", matchedParts: ["publisher"]},
+                {id: "2", authors: "author1", title: "title2", publisher: "some title", matchedParts: ["publisher"]},
+                {id: "3", authors: "author1", title: "test", publisher: "some", matchedParts: ["publisher"]},
+            ]
+        )
+
+        expect(getVisibleBooks(testBooks, "title")).toEqual(
+            [
+                {id: "1", authors: "author1", title: "title", publisher: "some", matchedParts: ["title"]},
+                {id: "2", authors: "author1", title: "title2", publisher: "some title", matchedParts: ["title", "publisher"]}
+            ]
+        )
+    })
+})
